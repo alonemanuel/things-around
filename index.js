6cc38bb1ca114c4bf690b018b3e7d52a10f3106c
alonemanuel <alon@alonemanuel.com>
2023-01-31 22:38:07 +0200

    added input insertion functionality

--- index.js
+++ index.js
@@ -139,69 +139,69 @@ let FADE_FACTOR = .8;
 
 let dots = [];
 let mouse = {
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0
 };
 
 var Dot = function () {
-    this.x = 0;
-    this.y = 0;
-    this.node = (function () {
-        let n = document.createElement(`div`);
-        n.className = `trail`;
-        document.body.appendChild(n);
-        return n;
-    }());
+  this.x = 0;
+  this.y = 0;
+  this.node = (function () {
+    let n = document.createElement(`div`);
+    n.className = `trail`;
+    document.body.appendChild(n);
+    return n;
+  }());
 };
 
 Dot.prototype.draw = function () {
-    this.node.style.left = `${this.x}px`;
-    this.node.style.top = `${this.y}px`;
+  this.node.style.left = `${this.x}px`;
+  this.node.style.top = `${this.y}px`;
 };
 
 
 function initDrawing() {
 
-    for (const i of Array(NUM_DOTS).keys()) {
-        let d = new Dot();
-        dots.push(d);
-    }
+  for (const i of Array(NUM_DOTS).keys()) {
+    let d = new Dot();
+    dots.push(d);
+  }
 
 
-    addEventListener(`mousemove`, function (event) {
-        mouse.x = event.pageX;
-        mouse.y = event.pageY;
-    });
+  addEventListener(`mousemove`, function (event) {
+    mouse.x = event.pageX;
+    mouse.y = event.pageY;
+  });
 
 
 }
 function draw() {
-    let lastX = dots.pe
-    // console.log(`Drawing...`);
-    let x = mouse.x;
-    let y = mouse.y;
-    // console.log(`x: ${x}, y: ${y}...`);
-    dots.forEach(function (dot, index, dots) {
-        let nextDot = dots[index + 1] || dots[0];
-        dot.x = x;
-        dot.y = y;
-        dot.draw();
-        x += (nextDot.x - dot.x) * FADE_FACTOR;
-        y += (nextDot.y - dot.y) * FADE_FACTOR;
-    });
+  let lastX = dots.pe
+  // console.log(`Drawing...`);
+  let x = mouse.x;
+  let y = mouse.y;
+  // console.log(`x: ${x}, y: ${y}...`);
+  dots.forEach(function (dot, index, dots) {
+    let nextDot = dots[index + 1] || dots[0];
+    dot.x = x;
+    dot.y = y;
+    dot.draw();
+    x += (nextDot.x - dot.x) * FADE_FACTOR;
+    y += (nextDot.y - dot.y) * FADE_FACTOR;
+  });
 }
 
 function animate() {
-    // console.log(`Animating...`);
-    draw();
-    requestAnimationFrame(animate);
+  // console.log(`Animating...`);
+  draw();
+  requestAnimationFrame(animate);
 }
 
 let addItem = function () {
-    var text = taskInput.value;
-    var li = document.createElement('li');
-    li.innerHTML = text;
-    incompleteTasks.appendChild(li);
+  var text = taskInput.value;
+  var li = document.createElement('li');
+  li.innerHTML = text;
+  incompleteTasks.appendChild(li);
 }
 
 var listDivId = 'mainThingsList';
@@ -211,73 +211,137 @@ var lastPressed;
 
 
 function setOnClick(clicked, elem, content) {
-    clicked.addEventListener('click', function (e) {
-        if (lastPressed) {
-            lastPressed.classList.remove('selected');
-        }
-        lastPressed = elem;
-        elem.classList.add('selected');
-        // e.currentTarget.style.color = 'red';
-        console.debug(content);
-        console.debug(content.style.display);
-        content.style.display = (getComputedStyle(content).display == 'none') ? 'block' : 'none';
-        // for (let child of content.children) {
-            // child.classList.add('selected');
-            // child.style.color = 'red';
+  clicked.addEventListener('click', function (e) {
+    if (lastPressed) {
+      lastPressed.classList.remove('selected');
+    }
+    lastPressed = elem;
+    elem.classList.add('selected');
+    // e.currentTarget.style.color = 'red';
+    console.debug(content);
+    console.debug(content.style.display);
+    content.style.display = (getComputedStyle(content).display == 'none') ? 'block' : 'none';
+    // for (let child of content.children) {
+    // child.classList.add('selected');
+    // child.style.color = 'red';
 
-            // alert(e.currentTarget.textContent);
-        // }
-    });
+    // alert(e.currentTarget.textContent);
+    // }
+  });
 
 }
 
 function initJsonObj(prevElem, jObj) {
-    // console.debug('\n');
-    if ('name' in jObj) {
-        console.debug(jObj["name"]);
+  // console.debug('\n');
+  if ('name' in jObj) {
+    console.debug(jObj["name"]);
 
-        let listItem = document.createElement(`li`);
-        let itemName = document.createElement(`h2`);
-        itemName.textContent = jObj["name"];
-        listItem.append(itemName);
-        prevElem.append(listItem);
-        
-        let itemContent = document.createElement('ul');
-        listItem.append(itemContent);
-        setOnClick(itemName, listItem, itemContent);
+    let listItem = prevElem.appendChild(document.createElement(`li`));
+    let itemName = listItem.appendChild(document.createElement(`h2`));
+    itemName.textContent = jObj["name"];
 
-        // Init input
-        let inputSpan = document.createElement(`span`);
-        inputSpan.classList.add('input-span');
-        let inputDiv = document.createElement(`li`);
-        inputDiv.append(inputSpan);
-        inputSpan.append(document.createElement(`input`));
-        itemContent.append(inputSpan);
+    let itemContent = listItem.appendChild(document.createElement('ul'));
+    setOnClick(itemName, listItem, itemContent);
 
-        if ('content' in jObj) {
-            for (let item of jObj["content"]) {
-                initJsonObj(itemContent, item);
-            }
+    // Init input
+    // let inputDiv = document.createElement(`li`);
 
-        }
+    // wage.addEventListener("keydown", function (e) {
+    //     if (e.code === "Enter") {  //checks whether the pressed key is "Enter"
+    //         validate(e);
+    //     }
+    // });
+
+    // function validate(e) {
+    //     var text = e.target.value;
+    //     //validation of the input...
+    // }
+
+
+    let inputSpan = itemContent.appendChild(document.createElement(`span`));
+    inputSpan.classList.add('input-span');
+    let inputDiv = inputSpan.appendChild(document.createElement(`input`));
+    inputDiv.addEventListener("keydown", function (e) {
+      if (e.key === "Enter") {
+        let childLi = prependEmptyChild(itemContent, e.target.value, true);
+        inputDiv.value = "";
+        // let newLi = itemContent.appendChild(document.createElement(`li`));
+        // newLi.textContent = "just added this";
+        // inputDiv.style.backgroundColor = "red";
+      }
+    });
+
+    if ('content' in jObj) {
+      for (let item of jObj["content"]) {
+        initJsonObj(itemContent, item);
+      }
 
     }
 
+  }
+
+}
+
+function appendAsSecond(parent, child) {
+  parent.insertBefore(child, parent.children[1]);
+  return child;
+}
+
+function prependChild(parent, child) {
+  parent.prepend(child);
+  return child;
+}
+
+function appendEmptyInput(parent) {
+  let inputSpan = parent.appendChild(document.createElement(`span`));
+  inputSpan.classList.add('input-span');
+  let inputDiv = inputSpan.appendChild(document.createElement(`input`));
+  inputDiv.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+      let childLi = prependEmptyChild(parent, e.target.value, true);
+      inputDiv.value = "";
+      // let newLi = itemContent.appendChild(document.createElement(`li`));
+      // newLi.textContent = "just added this";
+      // inputDiv.style.backgroundColor = "red";
+    }
+  });
+}
+
+const ITEM_CONTENT_INDEX = 1;
+function prependEmptyChild(parent, childTextContent, shouldInsertAsSecond) {
+  // let itemContent = parent.children[ITEM_CONTENT_INDEX];
+  let childLi;
+  if (shouldInsertAsSecond) {
+    childLi = appendAsSecond(parent, document.createElement('li'));
+  } else {
+    childLi = prependChild(parent, document.createElement('li'));
+  }
+  // console.debug('errrrr');
+  let childH2 = childLi.appendChild(document.createElement('h2'));
+  childH2.textContent = childTextContent;
+  let childItemContent = childLi.appendChild(document.createElement('ul'));
+  let emptyInput = appendEmptyInput(childItemContent);
+  setOnClick(childH2, childLi, childItemContent);
+
+  return childLi;
+  // let childItemContent =  
 }
 
 function initElements() {
-    let jsonObj = JSON.parse(data);
-    // console.debug(jsonObj);
+  let jsonObj = JSON.parse(data);
+  // console.debug(jsonObj);
 
-    // console.debug
-    initJsonObj(listDivElem, jsonObj);
+  // console.debug
+  initJsonObj(listDivElem, jsonObj);
 }
 
 
+
+
 function main() {
-    // initDrawing();
-    // animate();
-    initElements();
+  // initDrawing();
+  // animate();
+  initElements();
 
 }
 
